a23097db0f3924c5a7c2315e7eff26120e3670d7
jValdez <jdv_rodriguez@hotmail.com>
2020-09-11 11:43:45 -0500

    Fin del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ChartsModule } from 'ng2-charts';
 import { LineaComponent } from './components/linea/linea.component';
 import { BarraComponent } from './components/barra/barra.component';
 import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
 @NgModule({
   declarations: [
     AppComponent,
     LineaComponent,
     BarraComponent,
-    DonaComponent
+    DonaComponent,
+    RadarComponent
   ],
   imports: [
     BrowserModule,
